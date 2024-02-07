ca8cc78ace33dbbf0fa5da860423c29716ebe5f6
ArisaY <57202474+ArisaY-vs@users.noreply.github.com>
2024-02-07 17:17:49 +0900

    bugs

--- src/plans.js
+++ src/plans.js
@@ -47,7 +47,7 @@ function genPlanHtml(plan) {
         '<li>' + t('plans.minHeadCount', plan.minHeadCount) + '</li>' +
         '<li>' + plan.room + '</li>' +
       '</ul>' +
-      '<a href="./reserve.html?plan-id=' + plan.id + '" class="btn btn-primary" target="_blank" rel="opener">' + t('plans.reserveLink') + '</a>' +
+      '<a href="./index.html?plan-id=' + plan.id + '" class="btn btn-primary" target="_blank" rel="opener">' + t('plans.reserveLink') + '</a>' +
     '</div>' +
   '</div>' +
 '</div>';
